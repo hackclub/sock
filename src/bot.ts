d31ea278882891e3af67fbebed9f81b3a171a36f
Malted <malted@malted.dev>
2025-02-12 16:08:07 -0500

    Try processing before responding

--- src/bot.ts
+++ src/bot.ts
@@ -15,6 +15,7 @@ if (!process.env.EVENT_CHANNEL) {
 export const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_APP_SIGNING_SECRET,
+  processBeforeResponse: true,
 });
 
 await app.start();
